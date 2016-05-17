350bd18393c885d2eabf10c67677e8bd3684319d
Brandon Ramirez <brandon.ramirez@live.com>
2016-05-17 16:27:59 -0700

    same as last.

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,5 +1,5 @@
 export default {
   localhost: 'http://localhost:1234',
-  serverUrl: 'http://localhost:1234',
+  serverUrl: 'http://10.8.32.42:1234',
   // serverUrl: 'http://smileguess.herokuapp.com',
 };
